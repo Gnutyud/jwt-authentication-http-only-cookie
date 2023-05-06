408d077c21039f10c0521864526c041baf4f26f8
Gnutyud <nguyenduytungtd3@gmail.com>
2023-05-07 00:10:11 +0700

    persist login with refresh token

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
+import axios from "../config/axios";
 import useAuth from "../hooks/useAuh";
 
 const PrivateRoute = () => {
-  const { user } = useAuth();
+  const { user, login } = useAuth();
   const location = useLocation();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const verifyRefreshToken = async () => {
+      try {
+        const refreshResponse = await axios.get("/auth/refresh");
+        login(refreshResponse?.data?.user);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        if (isMounted) setLoading(false);
+      }
+    };
+
+    if (!user) {
+      verifyRefreshToken();
+    } else {
+      setLoading(false);
+    }
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  if (loading) return <h1>Loading...</h1>;
 
   return user ? (
     <Outlet />
